8f330bd9ce5673b257ff7fbce85ad5587409a8fd
corentin <corentin.crapart@ensg.eu>
2018-01-29 12:17:16 +0100

    Resolution fusion buttons et split

---
+++ buttons.js
@@ -0,0 +1,29 @@
+
+            // Inclinaison Sud
+            var button_sud = document.getElementById('button_sud');
+            button_sud.addEventListener("click", function(){
+              var current_pos=globeView.controls.getCameraTargetGeoPosition();
+			  console.log(current_pos);
+              globeView.controls.setCameraTargetGeoPosition({longitude:current_pos._values[0], latitude:current_pos._values[1]-45}, true)
+            });
+			// Inclinaison Nord
+            var button_nord = document.getElementById('button_nord');
+            button_nord.addEventListener("click", function(){
+              var current_pos=globeView.controls.getCameraTargetGeoPosition();
+			  console.log(current_pos);
+              globeView.controls.setCameraTargetGeoPosition({longitude:current_pos._values[0], latitude:current_pos._values[1]+45}, true)
+            });
+			// Inclinaison EST
+            var button_est = document.getElementById('button_est');
+            button_est.addEventListener("click", function(){
+              var current_pos=globeView.controls.getCameraTargetGeoPosition();
+			  console.log(current_pos);
+              globeView.controls.setCameraTargetGeoPosition({longitude:current_pos._values[0]+90, latitude:current_pos._values[1]}, true)
+            });
+			// Inclinaison OUEST
+            var button_OUEST = document.getElementById('button_OUEST');
+            button_OUEST.addEventListener("click", function(){
+              var current_pos=globeView.controls.getCameraTargetGeoPosition();
+			  console.log(current_pos);
+              globeView.controls.setCameraTargetGeoPosition({longitude:current_pos._values[0]-90, latitude:current_pos._values[1]}, true)
+            });
