72040646a0bba6dce0267e7a91777cf82ed5927b
b-yasmine <boudili.yasmina@gmail.com>
2018-01-29 14:54:56 +0100

    suppresion du tilt

--- gpx.js
+++ gpx.js
@@ -1,7 +1,6 @@
 itowns.GpxUtils.load('tmc.gpx', globeView.referenceCrs, {enablePin: true}).then(function (gpx) {
     if (gpx) {
         globeView.scene.add(gpx);
-        globeView.controls.setTilt(45, true);
-		console.log("GPX",gpx);
+		
     }
 });
